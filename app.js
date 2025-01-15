d6edc6aa9c8b54cd0e14df4fbc0aa3361f0869ab
Aysla <ayslaloureiro@gmail.com>
2025-01-15 18:55:32 +0000

    implementa app.js

--- app.js
+++ app.js
@@ -1,25 +1,38 @@
-const express = require('express');
+const express = require("express");
+const mongoose = require("mongoose");
+const userRoutes = require("./routes/users");
+const cardRoutes = require("./routes/cards");
 
+const { PORT = 3001 } = process.env;
 const app = express();
-const { PORT = 3000 } = process.env;
-const userRoutes = require('./routes/users');
-const cardRoutes = require('./routes/cards');
+
 // Middleware para parsing de JSON
 app.use(express.json());
 
-// Rota de teste
-app.get('/', (req, res) => {
-  res.json({ message: 'Server is running!' });
+mongoose.connect("mongodb://localhost:27017/aroundb");
+
+app.use((req, res, next) => {
+  req.user = {
+    _id: "6787f327b7cab77bd1597272", // cole o _id do usuário teste criado no passo anterior
+  };
+
+  next();
 });
 
-app.use((req, res) => {
-  res.status(404).json({ message: 'Router not found' });
+// Rota de teste
+app.get("/", (req, res) => {
+  res.json({ message: "Server is running!" });
 });
+
 // Rotas de usuários
-app.use('/users', userRoutes);
+app.use("/users", userRoutes);
 
 // Rotas de cards
-app.use('/cards', cardRoutes);
+app.use("/cards", cardRoutes);
+
+app.use((req, res) => {
+  res.status(404).json({ message: "Router not found" });
+});
 
 // Inicia o servidor
 app.listen(PORT, () => {
